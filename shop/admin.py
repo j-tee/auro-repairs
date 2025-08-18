243d6ccc71ae1c6f2907f8bd039eda732ba4ffc1
j-tee <juliustetteh@gmail.com>
2025-08-18 15:37:54 +0000

    Module registration in admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,24 +1,12 @@
-
+from django.apps import apps
 from django.contrib import admin
-from .models import Shop
+from typing import Type
 
-@admin.register(Shop)
-class ShopAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'phone', 'email', 'created_at')
-    list_filter = ('created_at', 'updated_at')
-    search_fields = ('name', 'phone', 'email', 'address')
-    ordering = ('-created_at',)
-    readonly_fields = ('created_at', 'updated_at')
-    
-    fieldsets = (
-        ('Basic Information', {
-            'fields': ('name', 'phone', 'email')
-        }),
-        ('Address', {
-            'fields': ('address',)
-        }),
-        ('Timestamps', {
-            'fields': ('created_at', 'updated_at'),
-            'classes': ('collapse',)
-        }),
-    )
+# Register all models from the shop app
+app_config = apps.get_app_config('shop')
+for model in app_config.get_models():
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
+        
